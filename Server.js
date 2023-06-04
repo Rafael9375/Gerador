c07d243011e9636a11efe7d1815ac7c71b7c35ec
Rafael Pereira de Paula <rafael.anexo@gmail.com>
2023-06-03 21:17:48 -0300

    Initial - Corrigindo o path

--- Server.js
+++ Server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const userRoutes = require('../Gerador/routes/userRoutes');
+const userRoutes = require('./routes/userRoutes');
 
 app.use(userRoutes);
 
